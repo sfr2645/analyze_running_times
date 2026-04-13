3f0dd47811ad4798935e28b6d520c91ca9def877
sfr2645 <sfr2645@email.vccs.edu>
2026-04-13 18:01:23 -0400

    changed algorithm.py name

--- analyze_running_times.py
+++ analyze_running_times.py
@@ -3,24 +3,21 @@ from algorithms import recursive_binary_search, iterative_binary_search, sequent
 arr = [3, 5, 8, 12, 14, 18, 21]
 arr.sort()
 
-target1 = 12  # Present
-target2 = 9   # Not present
+target1 = 12  
+target2 = 9 
 
-# Recursive Binary Search
 index = recursive_binary_search(arr, target1, 0, len(arr) - 1)
 print(f"{target1} found at index {index}" if index != -1 else f"{target1} not found")
 
 index = recursive_binary_search(arr, target2, 0, len(arr) - 1)
 print(f"{target2} found at index {index}" if index != -1 else f"{target2} not found")
 
-# Iterative Binary Search
 index = iterative_binary_search(arr, target1)
 print(f"{target1} found at index {index}" if index != -1 else f"{target1} not found")
 
 index = iterative_binary_search(arr, target2)
 print(f"{target2} found at index {index}" if index != -1 else f"{target2} not found")
 
-# Sequential Search
 index = sequential_search(arr, target1)
 print(f"{target1} found at index {index}" if index != -1 else f"{target1} not found")
 
